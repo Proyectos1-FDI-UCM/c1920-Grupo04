13a7905fbb2ce6b4008610d25dc976c3836ef35e
AlvaroCuerva <alcuer01@ucm.es>
2020-04-12 15:41:08 +0200

    Enemigo Espectro

---
+++ Assets/Scripts/MuertePorLuz.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuertePorLuz : MonoBehaviour
+{
+    //Este script lo tiene el enemigo Espectro, y es el que hace que muera en contacto con la luz
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Interact>() != null)     //Si el trigger tiene componente de interacción (solo el trigger de luz lo tiene)
+        {
+            Destroy(this.gameObject);                       //Se destruye
+        }
+    }
+}
