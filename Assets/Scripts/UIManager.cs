3d4a33cee714da9308bf87d3dd4b6a49c73e3859
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-04-20 14:17:28 +0200

    HUD
    Version final del HUD

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,8 +7,12 @@ public class UIManager : MonoBehaviour
 {
     public Image[] lives;
     public Image[] bat;
+    public Image bat7;
+    public Image bat8;
+    public Image batextra;
     public Text scoreText;
-    private int aux = 0; //Recordar cuantas vidas tenemos
+    private int aux = 0; //Recordar cuantas energias tenemos
+    private int maxbat = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,9 @@ public class UIManager : MonoBehaviour
 
     public void masEnergia(int maximo)
     {
+        maxbat = 8;
+        bat7.enabled = false; //Cambiar el fondo de las baterias
+        bat8.enabled = true;
         for(int i = maximo; aux != i; i--)//Devuelve a true solo los valores en false
         {
             bat[i].enabled = true;
@@ -35,25 +42,36 @@ public class UIManager : MonoBehaviour
     public void EnseñaVidas(int vidas)
     //Actualiza las vidas en el HUD
     {
-        if (vidas <= 4) lives[4].enabled = false;
-        if (vidas <= 3) lives[3].enabled = false;
-        if (vidas <= 2) lives[2].enabled = false;
+        if (vidas <= 2) lives[0].enabled = false;
         if (vidas <= 1) lives[1].enabled = false;
-        if (vidas <= 0) lives[0].enabled = false;
+        if (vidas <= 0) lives[2].enabled = false;
     }
 
     public void EnseñaBaterias(int baterias)
     //Actualiza las baterías en el HUD
     {
         aux = baterias;
-        if (baterias == 7) bat[7].enabled = false;
-        else if (baterias == 6) bat[6].enabled = false;
-        else if (baterias == 5) bat[5].enabled = false;
-        else if (baterias == 4) bat[4].enabled = false;
-        else if (baterias == 3) bat[3].enabled = false;
-        else if (baterias == 2) bat[2].enabled = false;
-        else if (baterias == 1) bat[1].enabled = false;
-        else if (baterias == 0) bat[0].enabled = false;
+        if (maxbat == 7)
+        {
+            if (baterias == 6) bat[6].enabled = false;
+            else if (baterias == 5) bat[5].enabled = false;
+            else if (baterias == 4) bat[4].enabled = false;
+            else if (baterias == 3) bat[3].enabled = false;
+            else if (baterias == 2) bat[2].enabled = false;
+            else if (baterias == 1) bat[1].enabled = false;
+            else if (baterias == 0) bat[0].enabled = false;
+        }
+        else
+        {
+            if (baterias == 7) batextra.enabled = false;
+            else if (baterias == 6) bat[6].enabled = false;
+            else if (baterias == 5) bat[5].enabled = false;
+            else if (baterias == 4) bat[4].enabled = false;
+            else if (baterias == 3) bat[3].enabled = false;
+            else if (baterias == 2) bat[2].enabled = false;
+            else if (baterias == 1) bat[1].enabled = false;
+            else if (baterias == 0) bat[0].enabled = false;
+        }
     }
 
     public void EnseñaPunt(int puntuacion)
