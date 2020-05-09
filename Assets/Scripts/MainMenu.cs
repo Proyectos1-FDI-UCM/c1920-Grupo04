d22bcf64c5c855e0983b34ad1d0d28133041bf4f
pablos16 <pablos16@ucm.es>
2020-05-09 19:45:20 +0200

    añadido audio mixer y que se pueda modificar el volumen
    he puesto un audioMixer para poder controlar el volumen del juego que se podra cambiar en el slider de opciones en la escena de menu principal pero me falta hacer que al regresar a la escena del menu el slider este en su sitio

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
-
+    public AudioMixer audioMixer;  
     public GameObject click;
     //este metodo es para empezar el juego al hacer que se cargue la escena Nivel 1 y tambien lo voy a usar para
     //cargar el primer nivel en el menu de seleccion de niveles
@@ -24,5 +25,9 @@ public class MainMenu : MonoBehaviour
     public void Sound() //Metodo al que hay que llamar en cada boton
     {
         Instantiate(click);
+    }
+    //este es el metodo que usa el slider para cambiar el volumen del juego
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("volume", volume);
     }
 }
