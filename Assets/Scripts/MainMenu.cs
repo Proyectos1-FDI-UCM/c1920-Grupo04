7071bc435f81a4e1abba1dc2618bd20f0f9adb70
pablos16 <pablos16@ucm.es>
2020-05-10 20:54:33 +0200

    Los cambios del slider del volumen se mantienen entre escenas
    ahora cuando se cambie el volumen se mantendran los cambios al cambiar de escena e incluso se mantendran al abrir y cerrar el programa

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider masterSlider;
     public AudioMixer audioMixer;  
-    public GameObject click;
+    
+
+
+    public void Start() {
+        //aqui uso PlayerPrefs para que recoja el volumen que tiene que tener el juego 
+        //para que el slider este con ese valor cuando se regrese a opciones
+        masterSlider.value = PlayerPrefs.GetFloat("volume", 0f);
+        audioMixer.SetFloat("volume", masterSlider.value);
+    }
+
     //este metodo es para empezar el juego al hacer que se cargue la escena Nivel 1 y tambien lo voy a usar para
     //cargar el primer nivel en el menu de seleccion de niveles
     public void NewGame() {
@@ -28,6 +39,10 @@ public class MainMenu : MonoBehaviour
     }
     //este es el metodo que usa el slider para cambiar el volumen del juego
     public void SetVolume(float volume) {
+        //aqui asigno el nuevo valor del volumen en PlayerPrefs para que se pueda acceder a el 
+        //en cualquier momento y que se mantenga entre escenas
+        PlayerPrefs.SetFloat("volume", volume);
         audioMixer.SetFloat("volume", volume);
+        
     }
 }
