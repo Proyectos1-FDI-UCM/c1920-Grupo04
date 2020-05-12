35be03c243c4e570e4b772a4984199717170a83b
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-05-12 16:00:00 +0200

    Textos
    Añadidos textos de ayuda y script, funcionan por cercania

---
+++ Assets/Scripts/text.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerController>())
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
