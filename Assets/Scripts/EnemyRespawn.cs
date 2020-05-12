737a4d57664c683adbbbcf65ecb62f41389438f5
diecasti <diecasti@ucm.es>
2020-05-12 16:12:13 +0200

    Scripts de respawn inicio
    Script que respawnea al enemigo si ha muerto, y añadido a vida enemigo un metodo booleano para comunicarle a este script que el enemigo esta muerto

---
+++ Assets/Scripts/EnemyRespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRespawn : MonoBehaviour
+{
+    public GameObject enemy;
+    bool dead = false;
+    public void Respawn()
+    {
+        if (dead)
+        {
+            Instantiate(enemy, gameObject.transform);
+            dead = false;
+        }
+    }
+    public void IsDead()
+    {
+        dead = true;
+    }
+}
--- Assets/Scripts/VidaEnemigo.cs
+++ Assets/Scripts/VidaEnemigo.cs
@@ -7,7 +7,11 @@ public class VidaEnemigo : MonoBehaviour
 {
     //Hay que declarar la vida de fomra publica
     public int vida;
-
+    GameObject respo;
+    private void Start()
+    {
+        respo = transform.parent.gameObject;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<VelBala>() != null)         //si la colisión era una bala
@@ -15,6 +19,8 @@ public class VidaEnemigo : MonoBehaviour
             vida--;
             if (vida <= 0)
             {
+                //tengo que cominicarle que se ha muerto al script padre para que leugo lo respawnee
+                respo.GetComponent<EnemyRespawn>().IsDead();
                 AudioManager.instance.PlaySound("morirenemigo", "play");
                 GameManager.instance.SumaPuntuacion(100);
                 Destroy(this.gameObject);   //Este objeto (Enemigo/muro) se destruye
