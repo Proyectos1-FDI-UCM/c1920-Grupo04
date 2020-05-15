f01660265b311d640a9fd830740f60b26589e7c0
PabloCubells <pablocub@ucm.es>
2020-05-15 03:15:20 +0200

    Arreglo del caida lenta
    No se bien que es lo que lo ha arreglado xD
    Pero probadlo a ver si tb os funciona bien y me decis jajaja
    He hecho 2 cosas:
    1. Un script de paracaidas, cuando estás a medio metro del suelo te frena la caida.
    (Funciona correctamente, pero lo he desactivado :D)
    2. Todo el tilemap de las paredes y suelos de la escena se encontraba bajo el layer "Default", lo que he hecho ha sido cambiarlo al layer "Escena", la cual acbo de crear.

---
+++ Assets/Scripts/Paracaidas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paracaidas : MonoBehaviour
+{
+    int layermask;
+    float distance;
+    Vector2 origin;
+
+    void Start()
+    {
+        origin = new Vector2(transform.position.x, transform.position.y);
+        distance = 0.5f;
+        layermask = 1 << 17;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 actualVel = gameObject.GetComponentInParent<Rigidbody2D>().velocity;
+        if (actualVel.y < -5)
+            if (Physics2D.Raycast(origin, Vector2.down, distance, layermask))
+            {
+                Vector2 newVel = new Vector2(actualVel.x, -2);
+                gameObject.GetComponentInParent<Rigidbody2D>().velocity = newVel;
+                Debug.Log("Paracaidas");
+            }
+    }
+}
