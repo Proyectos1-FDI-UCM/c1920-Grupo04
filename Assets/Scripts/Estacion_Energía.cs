4f5b446e6c5fef03bcc32abdc9e167d181b061b5
diecasti <diecasti@ucm.es>
2020-05-16 20:37:27 +0200

    Respawn bug fix
    solucionado error antes de que lo reporten ene l respawn, ya no respawnea bajo los limites del mapa

--- Assets/Scripts/Estacion_Energía.cs
+++ Assets/Scripts/Estacion_Energía.cs
@@ -6,10 +6,19 @@ public class Estacion_Energía : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D jugador)
     {
-        int cantidad = GameManager.instance.EnergiaParaSumar();
-        GameManager.instance.EnergiaSuma(cantidad);
-        Vector2 posi = new Vector2(jugador.transform.position.x, jugador.transform.position.y);
-        GameManager.instance.GuardaSpawn(posi);
-        GameManager.instance.RespawnEnemies();
+        if (jugador.GetComponent<PlayerController>())
+        {
+            int cantidad = GameManager.instance.EnergiaParaSumar();
+            GameManager.instance.EnergiaSuma(cantidad);
+            Vector2 posi = new Vector2(jugador.transform.position.x, jugador.transform.position.y); //guardamos la posicion del jugador 1 mas arriba porque da problemas con los pies
+            
+                                                                                                        //asi no los daria en teoria, sigue haciendolo
+                                                                                                        // el problema son los pies como hago que no interactue con los pies?
+                                                                                                        //pues una bolsa y al mercadona señora, estaria gucci tener las capas de colisiones ehh?
+
+            GameManager.instance.GuardaSpawn(posi);
+            GameManager.instance.RespawnEnemies();
+
+        }
     }
 }
