ca1134821e07d23c5fe92148e073d9d26e074596
PabloCubells <pablocub@ucm.es>
2020-05-18 04:19:50 +0200

    Más detecciones de colisiones
    Para que no se atasque al ir corriendo contra un muro sin saltar, antes era así:
    1. Corrias hacia el muro sin saltar.
    2. Una vez chocando saltabas continuando con el movimiento lateral.
    3. Casi no saltaba o se quedaba atascado.
    Además el delay entre salto normal y doble salto ahora sólo afecta al doble salto.
    El paracaidas activa el bool de si toca el suelo o no; si sobrepasa la velocidad, activa la caida lenta de seguridad.
    Arreglos tb en el tilemap de S2 y el acceso a S3.

--- Assets/Scripts/Paracaidas.cs
+++ Assets/Scripts/Paracaidas.cs
@@ -10,18 +10,33 @@ public class Paracaidas : MonoBehaviour
     void Start()
     {
         distance = 0.5f;
-        layermask = 1 << 17;
+        layermask = 1 << 17; //Hace una operación.
     }
 
     void FixedUpdate()
     {
         Vector2 actualVel = gameObject.GetComponentInParent<Rigidbody2D>().velocity;
-        if (actualVel.y < -5)
-            if (Physics2D.Raycast(transform.position, Vector2.down, distance, layermask))
+        if (Physics2D.Raycast(transform.position, Vector2.down, distance, layermask))
+        {
+            GameManager.instance.SueloTocado();
+            if (actualVel.y <= -5)
             {
                 Vector2 newVel = new Vector2(actualVel.x, -4);
-                gameObject.GetComponentInParent<Rigidbody2D>().velocity = newVel;
+                gameObject.GetComponentInParent<Rigidbody2D>().velocity = newVel;                
                 Debug.Log("Paracaidas");
             }
+        }
+        else
+            GameManager.instance.SueloFuera();
+    }
+
+    public bool CheckMove(Vector2 direction, bool check)
+    {
+        //bool check;
+        float distance = 1f;
+        int layermask = 1 << 17; //Hace una operación.
+        if (Physics2D.Raycast(transform.position, direction, distance, layermask))
+            check = true;
+        return check;
     }
 }
