0ed440e1670edb7ab1978781df10d38aa110664c
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-05-20 10:51:23 +0200

    Moneda implementada
    Creado script, animacion y sonido

---
+++ Assets/Scripts/moneda.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneda : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+            AudioManager.instance.PlaySound("Coin", "play");
+            GameManager.instance.SumaPuntuacion(50);
+        }
+    }
+}
