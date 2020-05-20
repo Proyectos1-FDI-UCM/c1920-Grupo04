e04f6d75f1a10eabbdb65d73d7eee96ee0a5a55a
diecasti <diecasti@ucm.es>
2020-05-20 13:30:34 +0200

    recarga bonita
    ahora las estaciones recargan el hud de forma mas dinamica y bonita, hope you like it guys

--- Assets/Scripts/Estacion_Energía.cs
+++ Assets/Scripts/Estacion_Energía.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class Estacion_Energía : MonoBehaviour
 {
+    private float timer;
+    private float cadencia = 0.1f;
+    private int bats = 0;
+
+    private void Start()
+    {
+        this.enabled = false;
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime; //Temporizador para limitar el uso de la bala
+        if (timer > cadencia)
+        {
+            GameManager.instance.EnergiaSuma(1);
+            timer = 0;
+            bats--;
+        }
+
+        if (bats <= 0)
+        {
+            this.enabled = false;
+        }
+
+    }
     private void OnTriggerEnter2D(Collider2D jugador)
     {
+
+
         if (jugador.GetComponent<PlayerController>())
         {
-            int cantidad = GameManager.instance.EnergiaParaSumar();
-            GameManager.instance.EnergiaSuma(cantidad);
-            Vector2 posi = new Vector2(jugador.transform.position.x, jugador.transform.position.y); //guardamos la posicion del jugador 1 mas arriba porque da problemas con los pies
+            bats = GameManager.instance.EnergiaParaSumar();
+            this.enabled = true;
+
+            Vector2 posi = new Vector2(jugador.transform.position.x, jugador.transform.position.y + 1); //guardamos la posicion del jugador 1 mas arriba porque da problemas con los pies
             
                                                                                                         //asi no los daria en teoria, sigue haciendolo
                                                                                                         // el problema son los pies como hago que no interactue con los pies?
-                                                                                                        //pues una bolsa y al mercadona señora, estaria gucci tener las capas de colisiones ehh?
+                                                                                                        //pues una bolsa y al mercadona señora.
 
             GameManager.instance.GuardaSpawn(posi);
             GameManager.instance.RespawnEnemies();
-
         }
     }
 }
