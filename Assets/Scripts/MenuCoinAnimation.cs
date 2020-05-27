af476f73185ebd999425886af227eb6891be999b
pablos16 <pablos16@ucm.es>
2020-05-27 02:27:21 +0200

    Añadido creditos, final del juego, final del nivel 1, añadido el nivel 2 a la selección de niveles del menu principal
    He arreglado un problema que ocurria al reiniciar el nivel desde el menu de pausa que hacia que el UI de la bateria se bugeara y no tenia la energia que tenia que tener al principio del nivel y he cambiado el texto del boton de resumir por resume porque no tenia sentido que estubiera en español un boton cuando el resto del juego esta en ingles.
    He añadido el final del nivel 1 que enseña el score del jugador que ha obtenido y hay un boton para ir al nivel 2.
    He añadido el final del juego que aparece al llegar al final del nivel 2 y enseña el score total del jugador y hay un boton para regresar al menu.
    En el menu pincipal he añadido los creditos del juego y ya hay un boton para ir al nivel 2 en la pantalla de seleccion de niveles del menu.

---
+++ Assets/Scripts/MenuCoinAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Este script es para animar la moneda en los creditos ya que no se pueden animar las imagenes del UI de la 
+//forma tradicional
+//No he hecho este script, lo he sacado de esta pagina: https://gist.github.com/almirage/e9e4f447190371ee6ce9
+//todo el crédito va para el creador del script
+public class MenuCoinAnimation : MonoBehaviour
+{
+    
+	public Sprite[] sprites;
+	public int spritePerFrame = 6;
+	public bool loop = true;
+	public bool destroyOnEnd = false;
+
+	private int index = 0;
+	private Image image;
+	private int frame = 0;
+
+	void Awake() {
+		image = GetComponent<Image>();
+	}
+
+	void Update() {
+		if (!loop && index == sprites.Length) return;
+		frame++;
+		if (frame < spritePerFrame) return;
+		image.sprite = sprites[index];
+		frame = 0;
+		index++;
+		if (index >= sprites.Length) {
+			if (loop) index = 0;
+			if (destroyOnEnd) Destroy(gameObject);
+		}
+	}
+
+}
