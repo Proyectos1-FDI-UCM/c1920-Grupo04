4125c80e74383f6efe9c87dc4f83b3646159aba5
PabloCubells <pablocub@ucm.es>
2020-05-27 04:43:51 +0200

    Cables (entradas/salidas)
    Tb he arreglado el bloque de S2 para que se vea mejor.
    Y agrandado las letras del minimapa.

--- Assets/Scripts/CableBlocked.cs
+++ Assets/Scripts/CableBlocked.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CableBlocked : MonoBehaviour
 {
-    public Material actived;
+    public Sprite actived;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.GetComponent<Disparo>() || collision.gameObject.GetComponent<VelBala>())
+        if(collision.gameObject.GetComponent<VelBala>())
         {
             this.gameObject.transform.GetChild(0).gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-            this.gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = actived;
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = actived;
         }
     }
 }
