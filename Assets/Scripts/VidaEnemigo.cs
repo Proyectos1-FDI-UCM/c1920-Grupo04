371de7e80052abf6a94b21accfdf0d7affcfbf9f
diecasti <diecasti@ucm.es>
2020-06-06 16:42:39 +0200

    Drop de baterias ya no se ejecuta en cambio de escenas
    Her eliminado el script de dropeo y he implementado su funcion al script de la vida de los enemigos

--- Assets/Scripts/VidaEnemigo.cs
+++ Assets/Scripts/VidaEnemigo.cs
@@ -8,8 +8,15 @@ public class VidaEnemigo : MonoBehaviour
     //Hay que declarar la vida de fomra publica
     public int vida;
     GameObject respo;
+    public int puntuacion = 100;
+    //Utiliza el metodo de Drop
+    public GameObject drop;  //game object público que se va a dropear (batería)
+    DisparoRobot scriptDisparo;
     private void Start()
     {
+    
+        //guarda en scriptDisparo si este gameObject (robot) tiene o no este script
+        scriptDisparo = this.gameObject.GetComponent<DisparoRobot>();
         respo = transform.parent.gameObject;
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -22,7 +29,29 @@ public class VidaEnemigo : MonoBehaviour
                 //tengo que cominicarle que se ha muerto al script padre para que leugo lo respawnee
                 respo.GetComponent<EnemyRespawn>().IsDead();
                 AudioManager.instance.PlaySound("morirenemigo", "play");
-                GameManager.instance.SumaPuntuacion(100);
+                GameManager.instance.SumaPuntuacion(puntuacion);
+                if (drop != null && GameManager.instance != null)
+                {
+                    int numDrop;
+                    //La manera de saber que robot se hace mirando si tiene o no el scriptDisparo
+                    if (scriptDisparo != null)          //si tiene scriptDisparo es un robot a distancia
+                    {
+                        numDrop = Random.Range(1, 3);   //por lo que dropea entre 1 y 2 (3 sin incluir)
+                    }
+                    else                                //si no tiene scriptDisparo no es un robot a distancia
+                    {                                   //(es un robot a melee)
+                        numDrop = Random.Range(0, 2);   //por lo que dropea entre 0 y 1 (2 sin incluir)
+                    }
+
+                    //DROPEO
+                    int cont = 0;
+                    while (cont < numDrop)
+                    {
+                        Vector3 posicion = new Vector2(this.transform.position.x, this.transform.position.y);
+                        Instantiate(drop, posicion, Quaternion.identity); //ESTÁ MAL
+                        cont++;
+                    }
+                }
                 Destroy(this.gameObject);   //Este objeto (Enemigo/muro) se destruye
             }
         }
